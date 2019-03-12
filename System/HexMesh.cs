75402cb7fd53662987817c8831b8cdb244d87a3e
John C <edibleleadpaint@gmail.com>
2019-03-12 19:18:10 -0400

    first commit - working hex grid

---
+++ System/HexMesh.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter),typeof(MeshRenderer))]
+public class HexMesh : MonoBehaviour
+{
+    Mesh hexMesh;
+    List<Vector3> vertices;
+    List<int> triangles;
+    List<Color> colors;
+
+    MeshCollider meshCollider;
+
+    void Awake()
+    {
+        GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
+        meshCollider = gameObject.AddComponent<MeshCollider>();
+        hexMesh.name = "Hex Mesh";
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+        colors = new List<Color>();
+    }
+
+
+    /* Triangulates All Cells */
+    public void TriangulateAll(HexCell[] cells){
+        hexMesh.Clear();
+        vertices.Clear();
+        triangles.Clear();
+        colors.Clear();
+
+        foreach(HexCell c in cells){
+            TriangulateCell(c);
+        }
+
+        hexMesh.vertices = vertices.ToArray();
+		hexMesh.triangles = triangles.ToArray();
+        hexMesh.colors = colors.ToArray();
+
+		hexMesh.RecalculateNormals();
+        meshCollider.sharedMesh = hexMesh;
+    }
+
+    /* creates the 6 triangles that comprise a hex cell */
+    void TriangulateCell(HexCell cell){
+        Vector3 center = cell.transform.localPosition;
+        for(int i = 0; i < 6; i++){
+            AddTriangle(
+                center,
+                center + HexMetrics.corners[i],
+                center + HexMetrics.corners[i + 1]
+            );
+            AddTriangleColor(cell.color);
+        }
+    }
+
+    /* Creates a triangle from three points and adds it to the lists */
+    void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3){
+        int vertexIndex = vertices.Count;
+        vertices.Add(v1);
+		vertices.Add(v2);
+		vertices.Add(v3);
+		triangles.Add(vertexIndex);
+		triangles.Add(vertexIndex + 1);
+		triangles.Add(vertexIndex + 2);
+    }
+
+    /* Colors a triangle */
+    void AddTriangleColor(Color c){
+        colors.Add(c);
+        colors.Add(c);
+        colors.Add(c);
+    }
+}
---
+++ System/HexMetrics.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class HexMetrics
+{
+    public const float outerRadius = 2f;
+    public const float innerRadius = outerRadius * 0.866025404f;
+
+    public static Vector3[] corners = {
+		new Vector3(0f, 0f, outerRadius),                       //north
+		new Vector3(innerRadius, 0f, 0.5f * outerRadius),       //northeast
+		new Vector3(innerRadius, 0f, -0.5f * outerRadius),      //southeast
+		new Vector3(0f, 0f, -outerRadius),                      //south
+		new Vector3(-innerRadius, 0f, -0.5f * outerRadius),     //southwest
+		new Vector3(-innerRadius, 0f, 0.5f * outerRadius),      //northwest
+		new Vector3(0f, 0f, outerRadius)                        //north again
+	};
+}
